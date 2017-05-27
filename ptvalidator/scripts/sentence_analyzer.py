b97ec157afd7a71bb4258f4fcee4e03d86f05dce
moskalap <przemyslaw.moskala@gmail.com>
2017-05-27 20:23:28 +0200

    Działa rozpoznawanie podmiotu w zdaniu + test

--- ptvalidator/scripts/sentence_analyzer.py
+++ ptvalidator/scripts/sentence_analyzer.py
@@ -1,41 +1,69 @@
-from spacy.symbols import nsubj, VERB
+from spacy.symbols import nsubj, VERB, NOUN, PROPN
 import spacy
-
+import logging
 
 class SentenceAnalyzer:
-    from enum import Enum
+    class Sentence:
+        def __init__(self, sentence_str, subject, predicate, verbs):
+            self.sentence = sentence_str
+            self.subject = subject
+            self.predicate = predicate
+            self.verbs = verbs
+        def get_similar(self):
+            pass
 
-    class Category(Enum):
-        PERSON = 1
-        COUNTRIES = 2
-        BUILDINGS = 3
+        def find_verbs(self):
 
-    def __init__(self, sentence):
+            verbs = set()
+            for possible_subject in self.sentence:
+                if possible_subject.dep == nsubj and possible_subject.head.pos == VERB:
+                    verbs.add(possible_subject.head)
+            return verbs
+
+        def get_subject(self):
+            subject = set()
+            for possible_subject in self.sentence:
+
+                if possible_subject.dep == nsubj:
+                    subject.add(possible_subject)
+
+            subject = list(subject)
+            returns = subject[0].text
+            if len(subject) > 1:
+                returns += ' ' + subject[1].text
+
+            return returns
+
+        def get_category(self):
+            return 'person'
+
+        def get_string(self):
+            return self.sentence_s
+
+    def __init__(self):
+
+        logging.info('init sentence analyzer')
+        self.nlp = spacy.load('en')
+
+    def parse_sentence(self, sentence):
+        doc = self.nlp(sentence)
+        subject_temp = None
+        subject_m = ""
+        for word in doc:
+            #print('text', word.text, 'pos', word.pos_, 'dep', word.dep_, 'head', word.head.text)
+            if word.dep == nsubj:
+                subject_temp = word.text
+        if not subject_temp:
+            for word in doc:
+                print('text', word.text, 'pos', word.pos_, 'dep', word.dep_, 'head', word.head.text)
 
 
-        nlp = spacy.load('en')
-        self.sentence = nlp(sentence)
-
-    def find_verbs(self):
-
-        verbs = set()
-        for possible_subject in self.sentence:
-            if possible_subject.dep == nsubj and possible_subject.head.pos == VERB:
-                verbs.add(possible_subject.head)
-        return verbs
-    def find_subject(self):
-        subject = set()
-        for possible_subject in self.sentence:
-            if possible_subject.dep == nsubj:
-                subject.add(possible_subject)
-        return subject
-
-    def_
+        for word in doc:
+            if word.head.text == subject_temp and word.pos == PROPN:
+                subject_m+=word.text+' '
+        subject_m += subject_temp
+        return self.Sentence(sentence,subject_m,"","")
 
 
 
-if __name__ == '__main__':
 
-    sa = SentenceAnalyzer('yesterday donald become president')
-    v =sa.find_subject()
-    print(v)
--- ptvalidator/tests/test_sentence_Analyzer.py
+++ ptvalidator/tests/test_sentence_Analyzer.py
@@ -1,7 +1,26 @@
 import ptvalidator.scripts.sentence_analyzer as sa
-sentence = sa.SentenceAnalyzer('donald trump is president of usa')
 
-def test_sentence():
-    verbs =  sentence.find_verbs()
-    verbs = list(verbs)
-    assert verbs[0].lower_ == 'is'
+
+def test_sentence_parser():
+    sentences = {
+        'Donald Trump is president of USA' : {'subject': 'Donald Trump'},
+        'Ariana Grande dies at manchester.' : {'subject' : 'Ariana Grande'},
+        'World War II was in 1940' : {'subject': 'World War II'},
+        'Audi is russian car' : {'subject': 'Audi'},
+        'Short hair is cool' : {'subject': 'hair'},
+        'Tram is the fastest vehicle' : {'subject': 'Tram'},
+        'The sun is a star' : {'subject': 'sun'},
+        'Yogurt is a dairy product' : {'subject': 'Yogurt'},
+        'The US has the largest military in the world' : {'subject': 'US'},
+        'Putin is a jerk' : {'subject': 'Putin'},
+        'Vladimir Putin is a jerk' : {'subject': 'Vladimir Putin'},
+        'Cats are better than dogs' : {'subject': 'Cats'},
+        'May 22nd is the best day of the year' : {'subject': 'May 22nd'},
+
+    }
+
+    sentence_analyzer = sa.SentenceAnalyzer()
+    for k in sentences.keys():
+        assert sentence_analyzer.parse_sentence(k).subject == sentences[k]['subject']
+
+
