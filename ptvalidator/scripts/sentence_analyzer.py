1e077e9cf237f684cd4108d22065e9cdc9826952
moskalap <przemyslaw.moskala@gmail.com>
2017-06-04 20:57:49 +0200

    Dodano analizator zdań

--- ptvalidator/scripts/sentence_analyzer.py
+++ ptvalidator/scripts/sentence_analyzer.py
@@ -1,7 +1,7 @@
-from spacy.symbols import nsubj, VERB, NOUN, PROPN, root, neg, ADV
+import logging
 
 import spacy
-import logging
+from spacy.symbols import nsubj, NOUN, PROPN, dobj, iobj, pobj, nsubjpass, acomp
 
 
 class SentenceAnalyzerException(Exception):
@@ -9,92 +9,95 @@ class SentenceAnalyzerException(Exception):
 
 
 class SentenceAnalyzer:
-    class Sentence:
-        def __init__(self, sentence_str, subject, verb, verbs, negated):
-            self.sentence = sentence_str
-            self.subject = subject
-            self.verb = verb
-            self.verbs = verbs
-            self.negated = negated
-        def get_opposite(self):
-            if self.negated:
-                list = self.sentence.split()
-                list.pop(self.negated)
-
-
-
-                return SentenceAnalyzer.Sentence(' '.join(list), self.subject, self.verb, self.verbs, False)
-
-            else:
-                list = self.sentence.split()
-                index = list.index(self.verb)
-                list.insert(index+1, 'not')
-                return SentenceAnalyzer.Sentence(' '.join(list),self.subject,self.verb,self.verbs,index+1)
-
-
-
-        def find_verbs(self):
-
-            verbs = set()
-            for possible_subject in self.sentence:
-                if possible_subject.dep == nsubj and possible_subject.head.pos == VERB:
-                    verbs.add(possible_subject.head)
-            return verbs
-
-        def get_subject(self):
-            subject = set()
-            for possible_subject in self.sentence:
-
-                if possible_subject.dep == nsubj:
-                    subject.add(possible_subject)
-
-            subject = list(subject)
-            returns = subject[0].text
-            if len(subject) > 1:
-                returns += ' ' + subject[1].text
-
-            return returns
-
-        def get_category(self):
-            return 'person'
-
-        def get_string(self):
-            return self.sentence_s
-
     def __init__(self):
-
-        logging.info('init sentence analyzer')
         self.nlp = spacy.load('en')
 
-    def parse_sentence(self, sentence):
-        doc = self.nlp(sentence)
+    @staticmethod
+    def compare_sentence(basic_sentence, other_sentence):
+        hashtags = list(map(lambda x: x.lemma_, basic_sentence.essential_htgs))
+        words_basic = list(map(lambda x: x.lemma_, basic_sentence.doc_sentence))
+        required = basic_sentence.required_ht
+        matched = 0
+        matched_htgs = 0
+        all_words = 0.01
+        for word in other_sentence.doc_sentence:
+            if word.lemma_ in hashtags:
+                matched_htgs += 1
+                matched += 1
+            elif word.lemma_ in words_basic:
+                matched += 1
+            all_words += 1
+
+        if matched_htgs < required:
+            return 0.0
+        else:
+            return matched / all_words
+
+    def create_sentence(self, sentence_string):
+        return Sentence(self.nlp(sentence_string))
+
+
+class Sentence:
+    def __init__(self, doc_sentence, subject=None):
+        if subject is None:
+            subject = []
+        self.doc_sentence = doc_sentence
+        self.essential_htgs = []
+        self.required_ht = 0
+        self.subject = subject
+
+    def get_subject(self):
         subject_temp = None
-        subject_m = ""
-        verb = None
-        for word in doc:
-            if word.dep == nsubj and (word.pos == NOUN or word.pos == PROPN):
-                subject_temp = word.text
-            if word.pos == VERB and word.head.text == word.text:
-                verb = word.text
-        negated = False
+        for word in self.doc_sentence:
+            print(word, word.dep_, word.pos_)
+            if (word.dep == nsubjpass or word.dep == nsubj) and (word.pos == NOUN or word.pos == PROPN):
+                subject_temp = [word]
 
-        if not subject_temp or not verb:
+        if subject_temp:
+            for word in self.doc_sentence:
+                if word.head.text == subject_temp[0].text and word.pos == PROPN:
+                    subject_temp = [word] + subject_temp
 
-            msg = ""
-            for word in doc:
-                msg+= str(('text', word.text, 'pos', word.pos_, 'dep', word.dep_, 'head', word.head.text))+'\n'
-                logging.error(msg)
-                raise SentenceAnalyzerException(msg)
+        return subject_temp
 
+    def get_essential_hashtag(self):
+        if not self.essential_htgs:
+            for word in self.doc_sentence:
+                if word.text.isdigit():
+                    self.essential_htgs.append(word)
+                elif word.dep == pobj:
+                    self.essential_htgs.append(word)
+                elif word.dep == iobj:
+                    self.essential_htgs.append(word)
+                elif word.dep == dobj:
+                    self.essential_htgs.append(word)
+                elif word.dep == acomp:
+                    self.essential_htgs.append(word)
+            if len(self.essential_htgs) > 4:
+                self.required_ht = 2
+            elif len(self.essential_htgs) != 0:
+                self.required_ht = 1
 
-        for word in doc:
-            if word.head.text == subject_temp and word.pos == PROPN:
-                subject_m+=word.text+' '
-            if word.dep == neg and (word.head.text == verb or word.pos == ADV):
-                negated = sentence.split().index(word.text)
-        subject_m += subject_temp
-        return self.Sentence(sentence,subject_m,verb,verb + sentence.split(verb)[1], negated)
+    def get_google_queries(self):
+        subjs = list(map(lambda x: x.lemma_, self.get_subject()))
 
+        self.get_essential_hashtag()
+        for es in self.essential_htgs:
+            subjs = list(map(lambda x: x + ' ' + es.lemma_, subjs))
 
+        subjs = [self.doc_sentence.string] + subjs
+        logging.info('google queries ' + str(subjs))
+        return subjs[:5]
 
+    def get_twitter_queries(self):
+        subjs = list(map(lambda x: '#' + x.lemma_, self.get_subject()))
+        if len(subjs) > 1:
+            subjs.append(' '.join(subjs))
 
+        self.get_essential_hashtag()
+        for es in self.essential_htgs:
+            subjs += list(map(lambda x: x + ' #' + es.lemma_, subjs))
+
+        subjs.append(self.doc_sentence.string)
+        logging.info('twitter queries = ' + str(subjs))
+        return subjs
