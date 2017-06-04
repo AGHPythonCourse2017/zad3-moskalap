927081570417e19a4ed1e12c5d6236af61cd2ddd
moskalap <przemyslaw.moskala@gmail.com>
2017-06-04 21:16:58 +0200

    dodano plik setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ptvalidator',
+    version='',
+    packages=['ptvalidator', 'ptvalidator.tests', 'ptvalidator.scripts'],
+    url='',
+    license='',
+    author='przemek',
+    author_email='',
+    description='', requires=['spacy', 'google', 'newspaper', 'twython', 'Crypto']
+)
